fc66047dbd58ca8950cc9e440d6b2b0b13f1cbd5
王天伦 <1574041371@qq.com>
2023-10-10 23:38:55 +0800

    second commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,6 +6,8 @@ Page({
      */
     data: {
         current:0,
+        bannerArr:[],
+        listArr:[],
     },
     //轮播图下标修改
     swiperChange:function(e){
@@ -18,10 +20,50 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad:function(options) {
+        wx.showLoading({
+          title: 'data loading',
+        })
+        var that=this;
         wx.request({
           url: 'http://iwenwiki.com:3002/api/banner',
           success:function(res){
-              console.log(res.data);
+              if(res.data.status == 200)
+              {
+                  wx.showToast({
+                    title: 'geted data',
+                  })
+                  console.log(res.data.data);
+                  console.log('请求成功');
+                  that.setData({
+                      bannerArr:res.data.data
+                  })
+              }
+              else
+              {
+                    console.log('请求失败');
+              }
+          },
+          complete:function(){
+              wx.hideLoading();
+          }
+        })
+        //获取首页数据
+        wx.request({
+          url: 'http://iwenwiki.com:3002/api/indexlist',
+          success:function(res){
+              //get list data
+              if(res.data.status == 200){
+                wx.showToast({
+                    title: 'geted data',
+                  })
+                console.log(res.data);
+                that.setData({
+                    listArr:res.data.data
+                })
+              }
+          },
+          complete:function(){
+              wx.hideLoading();
           }
         })
     },
